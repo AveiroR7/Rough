39c6931d33ed85f1990c1e969d6ccdc05a8e7574
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2021-05-19 17:40:09 +0530

    Update inheritance.java

--- inheritance.java
+++ inheritance.java
@@ -1,11 +1,11 @@
-class parent{
+class parent{  // base class
 	
 	public void func() {
 		System.out.println("This is parent Class");
 	}
 }
 
-class child extends parent{
+class child extends parent{  // derived class
 	
 	public void method() {
 		System.out.println("This is Child Class with additional feature");
@@ -19,7 +19,7 @@ public class inheritance {
 
 	public static void main(String[] args) {
 		
-		child obj = new child();
+		child obj = new child();  // creating new object and allocating memory.
 		obj.func();
 		obj.method();
 		
