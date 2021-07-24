2af2b9d5b6ca002a2b9bed91ef3367eabbda4b1f
Rahul Wasnik <1713322+FeurialBlack@users.noreply.github.com>
2021-07-24 07:29:58 +0530

    Create swapNumbers.java

---
+++ swapNumbers.java
@@ -0,0 +1,27 @@
+package interview;
+
+public class swapNumbers {
+
+	
+	public static void main(String args[]) {
+		
+		int first = 10;
+		int second = 20;
+		
+		System.out.println("Numbers Before Swapping :"+first+" "+second);
+		
+		// logic 2: 
+//		first = first+second; 
+//		second = first - second; 
+//		first = first - second;
+		
+		
+		// logic 3: 
+//		first = first*second;
+//		second = first/second; 
+//		first = first / second;
+		
+		
+		System.out.println("Numbers After Swapping :"+first+" "+second);
+	}
+}
