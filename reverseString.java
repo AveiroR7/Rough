1121b160e07af80d031f97c274bf6e9c868d0965
Rahul Wasnik <1713322+FeurialBlack@users.noreply.github.com>
2021-07-24 07:10:48 +0530

    Create reverseString.java

---
+++ reverseString.java
@@ -0,0 +1,44 @@
+package interview;
+public class reverseString {
+	
+	public static void main(String args[]) {
+	
+		String str = "ABCD";
+		
+		// METHOD 1 - USING STRING CHARAT and CONCATINATION METHOD
+
+//		String rev = "";
+//		
+//		int len = str.length();
+//		
+//		for(int i=len-1 ; i >=0 ; i--)
+//		{
+//			rev = rev+str.charAt(i);
+//			
+//		}
+//		
+//		System.out.println(rev);
+		
+		
+		// METHOD 2 - USING CHARACTER ARRAY
+		
+
+//		String rev = "";
+//		char array[] = str.toCharArray();
+//		int len = array.length;
+//		for(int i = len-1; i >=0; i--)
+//		{
+//			rev = rev + array[i];
+//		}
+//		
+//		System.out.println(rev);
+		
+		
+		// METHOD 3 using STRINGBUFFER CLASS
+		
+//		
+//		StringBuffer s = new StringBuffer(str);
+//		System.out.println(s.reverse());
+		
+	}
+}
